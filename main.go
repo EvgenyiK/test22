30cc233b1ffb86a327dabf336a28b0d916e6d295
Evgen <evgenprogrammer@gmail.com>
2020-10-13 22:05:40 +0300

    продолжение

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
+	"sort"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -13,19 +14,18 @@ import (
 type Adress struct{
 	Req string `json:"req_type"`
 	Result string 
-	Data []interface{}	`json:"data"`
+	Data []Data	`json:"data"`
 }
 
-type Users struct{
+type Data struct{
 	Item string `json:"item"`
 }
 
 
-var adress []Adress
-
 
 func createAdress(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "application/json")
+	defer r.Body.Close()
 	var ad Adress
 	err:=json.NewDecoder(r.Body).Decode(&ad)
 	if err != nil {
@@ -35,32 +35,46 @@ func createAdress(w http.ResponseWriter, r *http.Request)  {
 		ad.Result = "success"
 	}
 	
-	
-	//var us Users
-
-	for _, i := range ad.Data {
-		k,_ := json.Marshal(i)
-		fmt.Println(k)
+	var data []string
+	for _, v := range ad.Data {
+		data = append(data, fmt.Sprint(v))
 	}
-
 	
+	sort.Sort(Alphabetic(data))
+	fmt.Println()
+	
+	d:=strings.Join(data, ".....")
 
-	//fmt.Println(ad.Data)
+	fmt.Println(d)
+
 	json.NewEncoder(w).Encode(ad)
 }
 
-/*var state []string
+type Alphabetic []string
 
-func ToSlice(m map[string]interface{}) []state {
-	for k, v := range m {
-		v.
-	}
-}*/
+func (list Alphabetic) Len() int { return len(list) }
+
+func (list Alphabetic) Swap(i, j int) { list[i], list[j] = list[j], list[i] }
+
+func (list Alphabetic) Less(i, j int) bool {
+    var si string = list[i]
+    var sj string = list[j]
+    var si_lower = strings.ToLower(si)
+    var sj_lower = strings.ToLower(sj)
+    if si_lower == sj_lower {
+        return si < sj
+    }
+    return si_lower < sj_lower
+}
+
+
+
+
+
 
 func main()  {
 	r:= mux.NewRouter()
 	r.HandleFunc("/", createAdress).Methods("POST")
 	fmt.Println("start server")
-	log.Fatal(http.ListenAndServe(":8000", r))
-	
+	log.Fatal(http.ListenAndServe(":8008", r))
 }
